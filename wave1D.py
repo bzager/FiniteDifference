4043fa3b0fee34c17187a23921a10bc42f84350f
Ben Zager <bzager47@gmail.com>
2017-01-10 12:23:59 -0500

    updated some comments

--- wave1D.py
+++ wave1D.py
@@ -3,70 +3,79 @@
 # Finite difference method
 # u_tt = (c^2)*u_xx, 0<x<L
 # Boundary conditions: u(0,t) = 0, u(L,t) = 0
-# Initial conditions: u(x,0)=f(x), u_t(x,0)=g(x)
+# Initial conditions: u(x,0)=u0(x), u_t(x,0)=u_t0(x)
 # 
 
 import numpy as np
 from scipy import linalg
 import matplotlib.pyplot as plt
 from matplotlib import animation
-from JSAnimation import IPython_display 
 
+# Wave parameters
+c = 10.0 	# wave speed
+time = 10.0 # length of time domain
+L = 4.0 	# length of space domain
 
-nx = 201
-c = 10.0 # wave speed
-time = 10.0
-L = 4.0
-sigma = 0.8
-dx = L / nx
-dt = sigma*dx/c
-nt = int(time/dt)
-m = 3
-A = 1.0
+# Finite Difference parameters
+nx = 201 			# discrete space intervals
+sigma = 0.8 		# Courant number to satisfy CFL condition
+dx = L / nx 		# space step size
+dt = sigma*dx/c 	# time step size
+nt = int(time/dt) 	# discrete time intervals
 
+# Initial condition parameters
+m = 3 	# number of nodes in initial conditions
+A = 1.0 # amplitude of initial condition
+
+# returns initial value for u
 def u0Init(nx,A,L,m):
 	x = np.linspace(0,L,nx)
-	#u0 = A*np.sin(m*np.pi*x/L) + 0.7*A*np.sin((m+1)*np.pi*x/L) + 0.3*A*np.sin((m+5)*np.pi*x/L)
-	u0 = A*np.exp(-10*(x-0.2*L)**2) - A*np.exp(-10*(x-0.6*L)**2)
+	stand = A*np.sin(m*np.pi*x/L) # standing wave
+	gauss = A*np.exp(-10*(x-0.25*L)**2) # Gaussian
+	u0 = stand + gauss
+	#u0 = stand + 0.7*A*np.sin((m+2)*np.pi*x/L) + 0.3*A*np.sin((m+4)*np.pi*x/L)
+	#u0 = A*np.exp(-10*(x-0.2*L)**2) - A*np.exp(-10*(x-0.6*L)**2)
 
 	return u0
 
+# returns initial value for u_t
 def u_t0Init(nx,A,L,m):
 	x = np.linspace(0,L,nx)
-	u_t0 = np.zeros(nx)
+	u_t0 = np.zeros(nx) # no initial velocity
 	#u_t0 = A*np.cos(m*np.pi*x/L)
 	#u_t0 = 5*A*np.exp(-10*(x-0.3*L)**2)
 
 	return u_t0
 
 
-# ANIMATIONS
+# Animations
 def animate(data):
 	x = np.linspace(0,L,nx)
 	y = data
 	line.set_data(x,y)
 	return line,
 
-# 
+# Finite difference solver
 def wave1D(nt,dt,dx,c,L,A,m):
-	un = np.zeros((nt,nx))
-	u0 = u0Init(nx,A,L,m)
-	u_t0 = u_t0Init(nx,A,L,m)
-	rSq = (c*dt/dx)**2
+	un = np.zeros((nt,nx)) 		# initialize solution 
+	u0 = u0Init(nx,A,L,m)  		# get initial position
+	u_t0 = u_t0Init(nx,A,L,m) 	# get initial velocity
+	rSq = (c*dt/dx)**2 			# combine step sizes
 
+	# first time step, to account for initial velocity
 	un[0,:] = u0
 	un[1,1:-2] = un[0,1:-2] - dt*u_t0[1:-2] + 0.5*rSq*(un[0,2:-1]-2*un[0,1:-2]+un[0,0:-3])
-	un[1,0] = 0; un[1,-1] = 0;
+	un[1,0] = 0; un[1,-1] = 0; # Boundary conditions
 	
+	# rest of time steps
 	for i in range(1,nt-1):
 		un[i+1,1:-2] = 2*un[i,1:-2]-un[i-1,1:-2] + \
 				rSq*(un[i,2:-1]-2*un[i,1:-2]+un[i,0:-3])
 
-		un[i+1,0] = 0
+		un[i+1,0] = 0 # Boundary conditions
 		un[i+1,-1] = 0
 
 	return un
-
 
 
 fig = plt.figure()
